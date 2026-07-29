bc8a5d2fe478928d18e4f4f0365fb06e6b500f9f
MT.. <mtwkl9106@gmail.com>
2026-07-29 16:40:51 +0200

    Add main.dart for a simple Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('تواصل')),
+        body: Center(child: Text('شغال 100% 👋')),
+      ),
+    );
+  }
+}
